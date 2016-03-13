801800a35e67cbe7528ceb60c487be238e505187
LuboYankov <ahhak123@gmail.com>
2016-03-13 17:23:22 +0200

    #7 User registration - created user registration

---
+++ WebContent/scripts/users/signUp.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+	"use strict";
+	
+	var ENDPOINT = "http://localhost:3000/users";
+	
+	function checkEmail(email) {
+		var regExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		return regExp.test(email);
+	}
+	
+	function checkPassword(password, passwordConfirmation) {
+		if(password == passwordConfirmation) {
+			return true;
+		}
+		return false;
+	}
+	
+	function signUp() {
+		var user = {
+				username: $("[name='username']").val(),
+				name: $("[name='name']").val(),
+				email: $("[name='email']").val(),
+				phone: $("[name='phone']").val(),
+				password: $("[name='password']").val(),
+				favorites: []
+		}
+		
+		$.ajax(ENDPOINT, {
+			method: "POST",
+			contentType: "application/json; charset=utf-8",
+			data: JSON.stringify(user),
+			dataType: "json"
+		});
+	}
+	
+	function validForm() {
+		if(checkPassword($("[name='password']").val(), $("[name='confirm-password']").val()) & checkEmail($("[name='email']").val())) {
+			return true;
+		}
+		return false;
+	}
+	
+	function attachActionListeners() {
+		$(".user-action-sign-up").click(function() {
+			if(validForm()) {
+				signUp();
+				alert("Successfully created an account!");
+				// TODO redirect
+			} else {
+				alert("Incorrect information.");
+			}
+		});
+	}
+	
+	attachActionListeners();
+	
+});
