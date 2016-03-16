af541a0613ab034f101fedb3d54ddca8a56294c7
LuboYankov <ahhak123@gmail.com>
2016-03-16 22:03:50 +0200

    #16 Employer logout - created

--- WebContent/scripts/employers/profile.js
+++ WebContent/scripts/employers/profile.js
@@ -76,6 +76,11 @@ $(document).ready(function() {
 		$(document).on("click", "[data-job-id]", function() {
 			window.location.href = "../jobs/viewJob.html?id="+ $(this).attr("data-job-id");
 		});
+		
+		$("#logout").click(function() {
+			 document.cookie = 'session=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+			 window.location.href = "../index.html";
+		});
 	}
 	
 	fillEmployerInformation();
