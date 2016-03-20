7664c1644fffc6e575dc285f77a7eedd5036e376
LuboYankov <ahhak123@gmail.com>
2016-03-20 21:56:47 +0200

    #21 Admin logout - created

--- WebContent/scripts/admin/dashboard.js
+++ WebContent/scripts/admin/dashboard.js
@@ -186,6 +186,11 @@ $(document).ready(function() {
 		$("#admins-link").click(function() {
 			showAllAdmins();
 		});
+		
+		$("#logout").click(function() {
+			 document.cookie = 'session=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+			 window.location.href = "../index.html";
+		});
 	}
 	
 	getCurrentAdmin();
