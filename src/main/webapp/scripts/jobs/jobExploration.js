3d01008f22e98e49667a80ed7d9864e9fd0c49fc
LuboYankov <ahhak123@gmail.com>
2016-04-11 15:40:24 +0300

    #45 Sample search form - implemented

--- src/main/webapp/scripts/jobs/jobExploration.js
+++ src/main/webapp/scripts/jobs/jobExploration.js
@@ -11,32 +11,54 @@ $(document).ready(function() {
 		});
 	}
 	
+	function contains(string, substring) {
+		if(string.toUpperCase().indexOf(substring.toUpperCase()) > -1) {
+			return true;
+		}
+		return false;
+	}
+	
+	function showJobView(job) {
+		var divContainer = $("<div />");
+		var divThumbnail = $("<div />");
+		var divCaption = $("<div />");
+		var h4Title = $("<h4 />");
+		var h4Salary = $("<h4 />");
+		var pDescription = $("<p />");
+		divContainer.addClass("col-sm-4 col-lg-4 col-md-4");
+		divThumbnail.addClass("thumbnail");
+		divCaption.addClass("caption");
+		h4Salary.addClass("pull-right");
+		pDescription.text(job.description);
+		h4Title.text(job.title);
+		h4Salary.text("$"+job.salary);
+		divCaption.append(h4Salary);
+		divCaption.append(h4Title);
+		divCaption.append(pDescription);
+		divThumbnail.append(divCaption);
+		divContainer.attr("data-job-id", job.id);
+		divContainer.append(divThumbnail);
+		$(".col-md-9 .row").append(divContainer);
+	}
+	
+	function searchResults() {
+		var keywords = $("#search-keywords").val();
+		getJobs().then(function(jobs) {
+			$(".col-md-9 .row").html("");
+			_.forEach(jobs, function(job) {
+				if(contains(job.title, keywords)) {
+					showJobView(job);
+				}
+			});
+		});
+	}
+	
 	function showJobs() {
 		getJobs().then(function(response) {
-			function showJobView(job) {
-				var divContainer = $("<div />");
-				var divThumbnail = $("<div />");
-				var divCaption = $("<div />");
-				var h4Title = $("<h4 />");
-				var h4Salary = $("<h4 />");
-				var pDescription = $("<p />");
-				divContainer.addClass("col-sm-4 col-lg-4 col-md-4");
-				divThumbnail.addClass("thumbnail");
-				divCaption.addClass("caption");
-				h4Salary.addClass("pull-right");
-				pDescription.text(job.description);
-				h4Title.text(job.title);
-				h4Salary.text("$"+job.salary);
-				divCaption.append(h4Salary);
-				divCaption.append(h4Title);
-				divCaption.append(pDescription);
-				divThumbnail.append(divCaption);
-				divContainer.attr("data-job-id", job.id);
-				divContainer.append(divThumbnail);
-				$(".col-md-9 .row").append(divContainer);
-			}
 			$(".col-md-9 .row").html("");
-			_.forEach(response, showJobView);
+			_.forEach(response, function(value) {
+				showJobView(value);
+			});
 		});
 	}
 	
@@ -49,6 +71,10 @@ $(document).ready(function() {
 	function attachActionListeners() {
 		$(document).on("click", "[data-job-id]", function() {
 			window.location.href = "viewJob.html?id="+ $(this).attr("data-job-id");
+		});
+		
+		$("#search-button").click(function() {
+			searchResults();
 		});
 		
 		$("#logout").click(function() {
