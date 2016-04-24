60634f56091c3c4e63d449a6186da69755a596aa
LuboYankov <ahhak123@gmail.com>
2016-04-24 13:32:52 +0300

    Adding permissions for admin registration

--- src/main/java/org/elsysbg/ip/jobs/rest/AdministratorsRest.java
+++ src/main/java/org/elsysbg/ip/jobs/rest/AdministratorsRest.java
@@ -11,6 +11,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.elsysbg.ip.jobs.entities.Administrator;
 import org.elsysbg.ip.jobs.entities.SecurityRole;
 import org.elsysbg.ip.jobs.services.AdministratorsService;
@@ -28,6 +29,7 @@ public class AdministratorsRest {
 	@POST
 	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@RequiresPermissions("admin:registration")
 	public Administrator createAdministrator(Administrator administrator) {
 		administrator.setRole(SecurityRole.ADMINISTRATOR);
 		return administratorsService.createAdministrator(administrator);
